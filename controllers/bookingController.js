a2d8750069771557df84d0f24f5cdd307d64b4f2
Clil Argas <clilargas23@gmail.com>
2021-12-15 13:51:12 +0200

    Improved stripe implementations 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,7 +22,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`${req.protocol}://${req.get('host')}/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
